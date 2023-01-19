e0f69f6326b7834fde3cc1130ef48486439c4d02
Garik Kyarunts <garik@simplytechnologies.com>
2023-01-19 18:12:41 +0400

    second task

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const data = require("./data");
 
-const numbers = [205, 101, 118, 30, 48, 97, 11, 168];
+const numbersConstraintArray = [205, 101, 118, 30, 48, 97, 11, 168];
+const firstNameConstraint = "a";
 
-const result = data.filter(({ ip_address }) => {
+const constraints = {
+  numbers: numbersConstraintArray,
+  firstName: firstNameConstraint,
+};
+
+const result = data.filter(({ first_name, ip_address }) => {
+  if (first_name.toLowerCase().includes(constraints.firstName)) {
+    return false;
+  }
+
   const numbersFromIpAddress = ip_address.split(".");
   const helperObj = {};
 
   numbersFromIpAddress.forEach((ipNumber) => (helperObj[ipNumber] = true));
 
-  for (let i = 0; i < numbers.length; i++) {
-    if (helperObj[numbers[i]]) {
+  for (let i = 0; i < constraints.numbers.length; i++) {
+    if (helperObj[constraints.numbers[i]]) {
       return true;
     }
   }
